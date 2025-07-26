8d1dbf922af08ad63cd6fa44a49732409b9bfdf9
Siddhu kumar <sawhuman2008@gmail.com>
2025-07-27 01:04:03 +0530

    fifth commit

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import "../styles/HeroSection.css"
+import "../styles/heroSection.css"
 export default function HeroSection() {
   const slidesRef = useRef([]);
   const dotsRef = useRef([]);
